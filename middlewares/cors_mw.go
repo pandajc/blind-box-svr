acec410e435f2497ec345597fef029d1285c3dc9
romeoliu <liujuncao1994@163.com>
2023-11-03 16:56:18 +0800

    modified:   middlewares/cors_mw.go

--- middlewares/cors_mw.go
+++ middlewares/cors_mw.go
@@ -8,13 +8,16 @@ func Cors() gin.HandlerFunc {
         method := c.Request.Method
         origin := c.Request.Header.Get("Origin")
 		// todo 临时测试 生产要控制跨域
-		origin = "*"
+		if len(origin) == 0 {
+			origin = c.Request.Header.Get("Referer")
+		}
         if origin != "" {
             c.Header("Access-Control-Allow-Origin", origin)
             c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-            c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+            c.Header("Access-Control-Allow-Headers",
+			"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Methods, Access-Control-Allow-Origin")
             c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-            c.Header("Access-Control-Allow-Credentials", "false")
+            c.Header("Access-Control-Allow-Credentials", "true")
             c.Set("content-type", "application/json")
         }
         if method == "OPTIONS" {
